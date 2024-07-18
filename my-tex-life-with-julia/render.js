ba8295db90ea5569c1a11d8e9cc985e8ec152c8f
okatsn <okatsn@gmail.com>
2024-07-18 15:45:54 +0800

    render.js (2)

--- my-tex-life-with-julia/render.js
+++ my-tex-life-with-julia/render.js
@@ -2,15 +2,19 @@ const fs = require('fs');
 const mustache = require('mustache');
 
 // Read the template file
-const template = fs.readFileSync('template.mustache', 'utf8');
+const template_devcontainer = fs.readFileSync('devcontainer.mustache', 'utf8');
+const template_docker_compose = fs.readFileSync('docker-compose.mustache', 'utf8');
+
 
 // Read the data file
 const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
 // Render the template with the data
-const output = mustache.render(template, data);
+const output_devcontainer = mustache.render(template_devcontainer, data);
+const output_docker_compose = mustache.render(template_docker_compose, data);
 
 // Write the output to an HTML file
-fs.writeFileSync('output.html', output);
+fs.writeFileSync('.devcontainer/devcontainer.json', output_devcontainer);
+fs.writeFileSync('.devcontainer/docker-compose.yml', output_docker_compose);
 
-console.log('HTML file has been generated: output.html');
+console.log('Files has been generated');
