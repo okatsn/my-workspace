6d9dae6f8b02d599a0871285669dab59f070c9d2
okatsn <okatsn@gmail.com>
2025-02-04 11:18:17 +0800

    A if-else block to avoid unintented cp behaviors.

--- my-jupyter-with-julia/create_project.sh
+++ my-jupyter-with-julia/create_project.sh
@@ -5,12 +5,22 @@ newproj=$1
 # Navigate to the directory
 cd ./my-jupyter-with-julia || exit 1
 
-mkdir "$newproj"
+
+# Attempt to create the directory. 
+# If target directory already exists, exit and raise error to prevent the later unintended `cp` behaviors.
+if mkdir "$newproj"; then
+  echo "Directory '$newproj' created successfully."
+else
+  echo "Error: Failed to create directory '$newproj'." >&2
+  exit 1
+fi
+
+# The following `cp` commands will create an extra folder if there already is, for example, resulting in NewProject/.vscode/.vscode/settings.json if NewProject/.vscode already exists.
 
 node render.js
-cp -r ./.devcontainer "$newproj/.devcontainer"
-cp -r ./ltex-dictionary "$newproj/ltex-dictionary"
-cp -r ./.vscode "$newproj/.vscode"
-cp ./data.json "$newproj/data.json" 
+cp -r .devcontainer "$newproj/.devcontainer"
+cp -r ltex-dictionary "$newproj/ltex-dictionary"
+cp -r .vscode "$newproj/.vscode"
+cp data.json "$newproj/data.json" 
 
 cd ..
--- my-tex-life-with-julia/create_project.sh
+++ my-tex-life-with-julia/create_project.sh
@@ -2,11 +2,20 @@ newproj=$1
 
 # Navigate to the directory
 cd ./my-tex-life-with-julia || exit 1
-mkdir "$newproj"
+# Attempt to create the directory. 
+# If target directory already exists, exit and raise error to prevent the later unintended `cp` behaviors.
+if mkdir "$newproj"; then
+  echo "Directory '$newproj' created successfully."
+else
+  echo "Error: Failed to create directory '$newproj'." >&2
+  exit 1
+fi
+
+# The following `cp` commands will create an extra folder if there already is, for example, resulting in NewProject/.vscode/.vscode/settings.json if NewProject/.vscode already exists.
 
 node render.js
-cp -r ./.devcontainer "$newproj/.devcontainer"
-cp -r ./ltex-dictionary "$newproj/ltex-dictionary"
-cp ./data.json "$newproj/data.json" 
+cp -r .devcontainer "$newproj/.devcontainer"
+cp -r ltex-dictionary "$newproj/ltex-dictionary"
+cp data.json "$newproj/data.json" 
 
 cd ..
