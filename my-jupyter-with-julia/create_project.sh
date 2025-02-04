dce9207d58e9dee17b353e5aa2943de33a005922
okatsn <okatsn@gmail.com>
2025-02-04 09:44:41 +0800

    Consistent interface (fix incorrect context)

--- my-jupyter-with-julia/create_project.sh
+++ my-jupyter-with-julia/create_project.sh
@@ -1,15 +1,16 @@
 # Example: `. create_project.sh HelloWorld`
 
 newproj=$1
-mkdir "$newproj"
 
 # Navigate to the directory
 cd ./my-jupyter-with-julia || exit 1
 
+mkdir "$newproj"
+
 node render.js
-cp -r .devcontainer "$newproj/.devcontainer"
-cp -r ltex-dictionary "$newproj/ltex-dictionary"
-cp -r .vscode "$newproj/.vscode"
-cp data.json "$newproj/data.json" 
+cp -r ./.devcontainer "$newproj/.devcontainer"
+cp -r ./ltex-dictionary "$newproj/ltex-dictionary"
+cp -r ./.vscode "$newproj/.vscode"
+cp ./data.json "$newproj/data.json" 
 
 cd ..
--- my-tex-life-with-julia/create_project.sh
+++ my-tex-life-with-julia/create_project.sh
@@ -1,12 +1,12 @@
 newproj=$1
-mkdir "$newproj"
 
 # Navigate to the directory
 cd ./my-tex-life-with-julia || exit 1
+mkdir "$newproj"
 
 node render.js
-cp -r .devcontainer "$newproj/.devcontainer"
-cp -r ltex-dictionary "$newproj/ltex-dictionary"
-cp data.json "$newproj/data.json" 
+cp -r ./.devcontainer "$newproj/.devcontainer"
+cp -r ./ltex-dictionary "$newproj/ltex-dictionary"
+cp ./data.json "$newproj/data.json" 
 
 cd ..
