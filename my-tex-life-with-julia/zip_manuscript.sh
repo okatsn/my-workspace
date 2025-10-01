0088389a9c0b11e9570fc1da46331fc8d42f324c
okatsn <okatsn@gmail.com>
2025-10-01 11:59:14 +0800

    Add the zipping shell script.

---
+++ my-tex-life-with-julia/zip_manuscript.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Work in a temporary directory
+mkdir latex-manuscript/
+cp -r manuscript/. latex-manuscript/
+
+cd latex-manuscript
+
+
+. compile.sh output.tex
+
+# Remove auxiliary files before zipping (to avoid journal system to compile wrongly):
+find -type f -name '*.aux' -exec rm {} \;
+find -type f -name '*.blg' -exec rm {} \;
+find -type f -name 'main.*' -exec rm {} \;
+find -type f -name '*.sh' -exec rm {} \;
+find -type f -name '*.dvc' -exec rm {} \;
+rm .gitignore
+
+cd ..
+zip -9 -r latex-manuscript.zip latex-manuscript
+# the compression level from -0 (no compression) to -9 (highest compression).
+
+rm -rv latex-manuscript
