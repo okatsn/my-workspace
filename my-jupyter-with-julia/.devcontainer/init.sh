b49ca7da9ee407436453c5b46835d3b4abd8220d
okatsn <okatsn@gmail.com>
2025-10-17 15:34:27 +0800

    Use a `init.sh` file for julia initiation.

---
+++ my-jupyter-with-julia/.devcontainer/init.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+# The version is the first argument
+VERSION="$1"
+
+JULIA_ENV_DIR="$HOME/.julia/environments/$VERSION"
+JULIA_CONFIG_DIR="$HOME/.julia/config/"
+
+mkdir -p "$JULIA_ENV_DIR"
+mkdir -p "$JULIA_CONFIG_DIR"
+
+cp -f /tmp/julia/Project.toml "${JULIA_ENV_DIR}/Project.toml"
+cp -f /tmp/julia/startup.jl "${JULIA_CONFIG_DIR}/startup.jl"
+
+echo "Project.toml and startup.jl copied!"
+
+julia -e 'using Pkg; Pkg.update(); Pkg.instantiate(); Pkg.build("IJulia");'
+
+echo "Julia environment $VERSION instantiated complete"
